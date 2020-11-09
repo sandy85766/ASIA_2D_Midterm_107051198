ed2ec0d474363b0726fa0e798415ba164aa2c4b2
sandy85766 <71687733+sandy85766@users.noreply.github.com>
2020-11-09 15:40:57 +0800

    1109

---
+++ 2D_Midterm_107051198/Assets/Scripts/MenuManger.cs
@@ -0,0 +1,20 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MenuManger : MonoBehaviour
+{
+    public void Startgame()
+    {
+        print("開始遊戲");
+        SceneManager.LoadScene("關卡1");
+    }
+
+    public void Quitgame()
+    {
+        print("離開遊戲");
+        Application.Quit();
+    }
+
+}
---
+++ 2D_Midterm_107051198/Assets/Scripts/Player.cs
@@ -0,0 +1,14 @@
+﻿
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GameObject final;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        print("碰到傳送門ㄌ");
+    }
+    
+
+
+}
