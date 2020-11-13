ae92c0708a35be58ae7defeeafed620d4932442a
sandy85766 <71687733+sandy85766@users.noreply.github.com>
2020-11-13 15:29:33 +0800

    1113鯛魚燒

--- 2D_Midterm_107051198/Assets/Scripts/Player.cs
+++ 2D_Midterm_107051198/Assets/Scripts/Player.cs
@@ -1,12 +1,25 @@
 ﻿
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
     public GameObject final;
+    public Text textCount;
+    public int count;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        print("碰到傳送門ㄌ");
+        if (collision.name == "傳送門")
+        {
+            final.SetActive(true);
+        }
+
+        if (collision.tag == ("鯛魚燒"))
+        {
+            Destroy(collision.gameObject);
+            count++;
+            textCount.text = "鯛 魚 燒 數 量 : " + count;
+        }
     }
     
 
